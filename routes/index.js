a29c85cda911e434103bb7f49b36f1dae4b243c2
CjL95 <71044991+CjL95@users.noreply.github.com>
2021-08-30 01:35:55 -0400

    also added logout route

--- routes/index.js
+++ routes/index.js
@@ -128,6 +128,18 @@ router.get('/', function(req, res, next) {
   } 
     
 })
+.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  }
+  else {
+    var err = new Error('You are not logged in!');
+    err.status = 403;
+    next(err);
+  }
+})
 module.exports = router;
 
 
