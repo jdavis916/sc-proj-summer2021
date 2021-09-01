032492088a0445fbbaa35a2ae8106690b2be45b2
CjL95 <71044991+CjL95@users.noreply.github.com>
2021-09-01 14:49:19 -0400

    accidentally didnt commit everything

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,23 @@
 console.log("Script file connected");
+//changes login button to log out
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    /* if(!loggedIn){
+    	document.getElementById('#btnProf').style.visibility = "hidden";
+    } */
+	document.querySelector('#btnLogout').onclick = function logout(){
+		if(loggedIn){
+			this.innerHTML = 'Logout';
+		//true
+		   location.replace('/logout');
+		} else{
+		//false
+			this.innerHTML = 'Sign in';
+			location.replace('/login');
+		}
+	}
+
+	/* document.querySelector('#btnProf').onclick = function {
+		location.replace('/profile');
+	} */
+});
